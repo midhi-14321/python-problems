3771ecf2190c9eef8799ea937b3a7d74d46f58c2
midhi-14321 <sankamidhilesh@gmail.com>
2025-01-17 11:49:52 +0530

    file deleted

--- palindrome.py
+++
@@ -1,9 +0,0 @@
-#check if a given string is a palindrome.
-palindrome=input('enter a string: ')
-reverse=""
-for i in range(len(palindrome)-1,-1,-1):
-    reverse+=palindrome[i]
-if palindrome==reverse:
-    print('palindrome')
-else:
-    print('not a palindrome')
